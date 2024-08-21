119e15b2672e6f23da07cdb22ba1700153806b58
Anton Kheifets <39564160+metsl@users.noreply.github.com>
2024-08-21 17:57:43 +0300

    add Turtle game Day 18

---
+++ Day-18-Start.py
@@ -0,0 +1,24 @@
+import turtle as t
+import random
+timmy = t.Turtle()
+timmy.shape("turtle")
+
+colors = ["red", "blue", "yellow", "brown", "green", "purple", "orange", "green"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        timmy.forward(100)
+        timmy.right(angle)
+        
+for shape_side_n in range(3,11):
+    timmy.color(random.choice(colors))
+    draw_shape(shape_side_n)
+
+
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
