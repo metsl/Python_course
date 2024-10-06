a994e18151a663577577d189302720ec6acb6332
Anton Kheifets <39564160+metsl@users.noreply.github.com>
2024-10-06 16:47:38 +0300

    Ping Pong Completed

--- ping_pong/main.py
+++ ping_pong/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -21,7 +23,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
-    time.sleep(0.05)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -32,12 +34,14 @@ while game_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+
     # Detect miss
     if ball.xcor() > 400:
         ball.reset()
+        scoreboard.l_point()
     if ball.xcor() < -400:
         ball.reset()
-
+        scoreboard.r_point()
 
 
 screen.exitonclick()
